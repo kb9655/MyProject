51b1ab92d62239a49c80eb16d1e23bb7a00875e4
kb9655 <kkb9655@gmail.com>
2020-09-18 20:13:43 +0900

    week1

---
+++ week1/py_03_02_2016182003_1.py
@@ -0,0 +1,298 @@
+1+2
+
+100-1
+
+7*52
+
+1024/8
+
+1/2
+
+1//2
+
+4/3
+
+4//3
+
+7%3
+
+7.5%3
+
+4%2.1
+
+3.1415926535 * (3*3)
+
+3.1415926535 * 3**2
+
+3.141592 % 5.77
+
+4.56789%3.33
+
+pi = 3.1415926535
+
+pi *4 **2
+
+pi*2.5 **2
+
+pi * 8 ** 2
+
+print(pi)
+
+pi
+
+r=7
+
+area= pi *r **2
+
+print(area)
+
+area
+
+r=11
+
+area = pi *r **2
+
+area
+
+r**2 * pi
+
+name1 = "Trump"
+
+name2='강다니엘'
+
+print(name1)
+
+print(name2)
+
+order=4
+
+pi=3.141592
+
+name="Trump"
+
+type(order)
+
+type(pi)
+
+type(name)
+
+result=True
+
+type(result)
+
+type(name2)
+
+num1=5
+
+num2=2.5
+
+5/2.5
+
+num1//num2
+
+tmp=num1//num2
+
+type(tmp)
+
+4<3
+
+4 > 3
+
+3.5 < -1.3
+
+100==100
+
+10>=10
+
+T=365>366
+
+a
+
+type(a)
+
+first = "Daehyun"
+
+last="Lee"
+
+name = first+" "+last
+
+name
+
+print(name)
+
+name *2
+
+name * 3
+
+name[0]
+
+name[2]
+
+name[-1]
+
+name[-2]
+
+name[99]
+
+name[10]
+
+name[-3]
+
+a=1
+
+a[1]
+
+title = "Python 2D Game Programming"
+
+title[0:6]
+
+title[7:9]
+
+title[10:14]
+
+title[:6]
+
+title[-11:]
+
+title[::2]
+
+title[::-1]
+
+title[::-3]
+
+twice = ['momo', 'sana', 'zwi', 'nayun', 'dahyun']
+
+black_pink = ['jisu','jeni','rose','risa']
+
+twice
+
+twice.append('jihyo')
+
+ywice
+
+twice
+
+twice.sort()
+
+twice
+
+len(twice)
+unite = twice + black_pink
+
+unite
+
+unite.remove('momo')
+
+unite
+
+unite[0]
+
+unite[-1]
+
+unite[:3]
+
+unite[-3:]
+
+score = {'momo' : 80, 'zwi' : 85, 'sana' : 98}
+
+type(score)
+
+score['momo']
+
+
+score['nayun']
+
+ 
+score['nayun']
+
+score['nayun'] = 100
+
+score
+
+del score['momo']
+
+score
+
+score.keys()
+
+score.values()
+
+'zwi' in score
+
+'momo' in score
+
+score[1]
+
+score.clear()
+
+socre
+
+
+t1=(1,2,3)
+t2=(1, )
+t3=()
+t4=1,2,3,4
+t4
+
+type(t4)
+
+t5=(1,'a',"park",(1,2))
+t1[1:]
+
+t1+t5
+
+t4*t4
+
+t4*2
+
+type(t2)
+
+t2
+
+t1-t2
+
+t4[:-2]
+
+t4[-2:]
+
+tup=1,2,3,4
+type(tup)
+
+s1={1,2,3}
+type(s1)
+
+s1={1,2,2,4}
+s1
+
+l1=[1,2,2,2,2,3,3,3,3,5,5,5,5,5]
+s1 = set(l1)
+s1
+
+s2={3,5,6,7}
+s1+s2
+
+s1 | s2
+
+s1&s2
+
+s2-s1
+
+s1-s2
+
+s1.add(8)
+s1
+
+s2.remove(6)
+s2
+
+ 
+s3={4,3,2,1}
+s3
+
+t1
+
+s3=set(t1)
+s3
+
+s3[1]
+
